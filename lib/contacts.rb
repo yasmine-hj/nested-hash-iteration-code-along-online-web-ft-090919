7d5ac0873cd76506176396f6c767456446e2d1bb
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-14 09:45:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -22,10 +22,8 @@ def remove_strawberry(contacts)
         if attribute == :favorite_ice_cream_flavors
           data.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
-  end
-  end
+       end
+      end
     end
-  end
-  end 
   end
 end
