dd216087bf6b99132e651168de0acf9bb2cec4e7
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-14 09:43:01 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,9 +17,13 @@ def remove_strawberry(contacts)
   
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
+      
       contact_details_hash.each do 
         |attribute, data|
+        
+        if attribute == :favorite_ice_cream_flavors
     binding.pry
+  end
     end
   end 
   end
